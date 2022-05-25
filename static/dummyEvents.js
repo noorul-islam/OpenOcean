1776362e572c17501c798531414b79a3912c1758
Noorul Islam <noorul_islam@hotmail.com>
2022-05-25 18:55:10 +0530

    Initial commit

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 0.01,
+    from: '0xDavid',
+    to: 'noorul.eth',
+    date: '2 months ago',
+  },
+  {
+    price: 0.04,
+    from: '0xQazi',
+    to: 'noorul.eth',
+    date: '2 months ago',
+  },
+  {
+    price: 0.02,
+    from: 'param.eth',
+    to: 'noorul.eth',
+    date: '2 months ago',
+  },
+  {
+    price: 0.05,
+    from: '0xQazi',
+    to: 'noorul.eth',
+    date: '2 months ago',
+  },
+]
